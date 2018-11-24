34d89c53e1e7e85bcd561958178e88cb1bc8052e
Shemar Dacosta <dshemar6@gmail.com>
2018-11-24 23:23:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,10 @@
 
 def speak_to_grandma(phrase)
 
-if phrase == 
-  puts
-  
+if phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == phrase.downcase
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif phrase == phrase.upcase
+  puts "NO, NOT SINCE 1938"
 end
